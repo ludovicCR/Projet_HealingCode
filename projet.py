11943b4e342d84aee47981ae0ac4c91dd2530f70
Ludovic <ludovic.crousse@gmail.com>
2023-12-14 11:48:54 +0100

    Premier ébauche

---
+++ projet.py
@@ -0,0 +1,22 @@
+class patient():
+    def __init__(self, nom, maladie, argent, poche, etat__sante):
+        self.nom = nom
+        self.nom = maladie
+        self.nom = argent
+        self.nom = poche
+        self.nom = etat__sante
+
+    def se_deplacer(self):
+        print
+    def take_pills(self):
+        print
+    def payer(self):
+        print
+
+
+patients1 = patient ("Marcus","Mal indenté",100,[],"malade")
+patients2 = patient ("Optimus","unsave",200,[],"malade")
+patients3 = patient ("Sangoku","404",80,[],"malade")
+patients4 = patient ("DarthVader","Azmatique",110,[],"malade")
+patients5 = patient ("Semicolon","syntaxError",60,[],"malade")
+
