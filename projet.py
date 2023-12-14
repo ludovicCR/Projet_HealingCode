408183ec61c27fb4f36463a7630cc8beede50d81
Ludovic <ludovic.crousse@gmail.com>
2023-12-14 14:48:12 +0100

    ajout class doc et fonction

--- projet.py
+++ projet.py
@@ -27,3 +27,39 @@ optimus.afficher_info()
 sangoku.afficher_info()
 darth_vader.afficher_info()
 semicolon.afficher_info()
+
+class Docteur:
+    def __init__(self,nom,argent,cabinet,diagnostique,patient_in=None,patient_out=None):
+        self.nom = nom
+        self.argent = argent
+        self.cabinet = cabinet
+        self.diagnostique = diagnostique
+        self.patient_in = patient_in
+        self.patient_out = patient_out
+
+    def payer_consultation(self,patient):
+        patient.payer(50)
+        self.argent += 50
+    
+    def prescrire_traitement(self,patient):
+        grille_diagnostics = {
+            "mal indenté": "ctrl+maj+f",
+            "unsave": "saveOnFocusChange",
+            "404": "CheckLinkRelation",
+            "azmatique": "Ventoline",
+            "syntaxError": "f12+doc"
+        }
+
+        traitement = grille_diagnostics.get(patient.maladie)
+        if traitement:
+            print(f"{self.nom} prescrit le traitement {traitement} à {patient.nom}.")
+        else:
+            print(f"Aucun traitement trouvé pour la maladie de {patient.nom} :( )")
+            
+    def diagnostiquer(self, patient):
+        print(f"{self.nom} diagnostique {patient.nom}.")
+        patient.etat_sante = "En traitement"
+        self.payer_consultation(patient)
+        self.prescrire_traitement(patient)
+        self.patient_out = patient
+        self.patient_in = None
