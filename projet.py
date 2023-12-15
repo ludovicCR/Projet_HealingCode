f910c08fc2bd8503a106a226b87d98588c69fec4
Ludovic <ludovic.crousse@gmail.com>
2023-12-15 11:13:31 +0100

    Essai de fin

--- projet.py
+++ projet.py
@@ -1,4 +1,6 @@
-class patient():
+from tabulate import tabulate
+
+class Patient():
     def __init__(self, nom, maladie, argent, poche, etat_sante):
         self.nom = nom
         self.maladie = maladie
@@ -6,10 +8,27 @@ class patient():
         self.poche = poche
         self.etat_sante = etat_sante
 
-    def se_deplacer(self):
-        print
+    def se_rendre(self,endroit):
+        print (f"{self.nom} se rend à {endroit}.")
+        if endroit == "cabinet":
+            self.cabinet()
+        elif endroit == "pharmacie":
+            self.pharmacie()
+        elif endroit == "cimetiere":
+            self.cimetiere()
+    
+    def cabinet(self):
+        print(f"{self.nom} arrive au cabinet du docteur")
+
+    def pharmacie(self):
+        print(f"{self.nom} est arrivé à la pharmacie.")
+
+    def cimetiere(self):
+        print(f"{self.nom} est mort de sa maladie. Il est poussé au cimetière.")
+    
     def take_pills(self):
         print
+    
     def payer(self,montant):
         if self.argent >= montant:
             self.argent -= montant
@@ -17,20 +36,6 @@ class patient():
     
     def afficher_info(self):
         print(f"| {self.nom} | {self.maladie} | {self.argent} | {self.poche} | {self.etat_sante} |")
-#Initialisation
-marcus = patient ("Marcus","Mal indenté",100,[],"malade")
-optimus = patient ("Optimus","unsave",200,[],"malade")
-sangoku = patient ("Sangoku","404",80,[],"malade")
-darth_vader = patient ("DarthVader","Azmatique",110,[],"malade")
-semicolon = patient ("Semicolon","syntaxError",60,[],"malade")
-
-#Affichage informations patients
-print("| Nom    | Maladie      | Argent  | Poche   | Etat de santé    |")
-marcus.afficher_info()
-optimus.afficher_info()
-sangoku.afficher_info()
-darth_vader.afficher_info()
-semicolon.afficher_info()
 
 class Docteur:
     def __init__(self ,nom ,argent ,cabinet ,diagnostique ,patient_in=None ,patient_out=None ):
@@ -59,6 +64,7 @@ class Docteur:
             print(f"{self.nom} prescrit le traitement {traitement} à {patient.nom}.")
         else:
             print(f"Aucun traitement trouvé pour la maladie de {patient.nom} :( )")
+    
     def diagnostiquer(self, patient):
         print(f"{self.nom} diagnostique {patient.nom}.")
         patient.etat_sante = "En traitement"
@@ -66,22 +72,79 @@ class Docteur:
         self.prescrire_traitement(patient)
         self.patient_out = patient
         self.patient_in = None
+
     def sortie_patient(self):
         if self.patient_out:
             print(f"{self.nom} fait sortir {self.patient_out.nom} de son cabinet.")
             self.patient_out = None
+
         else:
             print("Aucun patient à faire sortir.")
 
+#Class pharma
+            
+class Pharmacie:
+    def __init__(self):
+        self.tarifs_traitements = {
+            "ctrl+maj+f": 55,
+            "saveOnFocusChange": 75,
+            "CheckLinkRelation": 35,
+            "Ventoline": 40,
+            "f12+doc": 20
+
+        }
+    def vendre_traitement(self, patient):
+        traitement = patient.poche
+        if traitement in self.tarifs_traitements:
+            prix_traitement = self.tarifs_traitements[traitement]
+            if patient.argent >= prix_traitement:
+                print(f"{patient.nom} achète le traitement {traitement} à {prix_traitement}€ à la pharmacice.")
+                patient.argent -= prix_traitement
+                print(f"{patient.nom} a maintenant {patient.argent}€")
+                print (f"{patient.nom} prend le traitement et son état de santé s'améliore")
+                return True
+            else:
+                print(f"{patient.nom} n'a pas assez d'argent pour acheter le traitement {traitement}.")
+                print(f"{patient.nom} est considéré mort et poussé au cimetière.")
+                return False
+        else:
+            print(f"Aucun tarif trouvé pour {traitement}.")
+
+# Initialisation des patients
+patients = [
+    Patient("Marcus","mal indenté",100,"Vide","malade"),
+    Patient("Optimus","unsave",200,"vide","malade"),
+    Patient("Sangoku","404",80,"vide","malade"),
+    Patient("DarthVader","azmatique",110,"Vide","malade"),
+    Patient("semicolon","syntaxError",60,"vide","malade"),
+]
+
 debugger = Docteur("Debugger", 0, ["chat"], "-")
 
-#Affichage
+pharmacie = Pharmacie()
+
+# Simulation de l'achat de traitement par les patients
+traitements_patients = ["ctrl+maj+f", "saveOnFocusChange", "CheckLinkRelation", "Ventoline", "f12+doc"]
+
+print("| Nom       | Maladie         | Argent | Poche | État de Santé |")
+print("|-----------|-----------------|--------|-------|---------------|")
+patient_data = [[patient.nom, patient.maladie, patient.argent, patient.poche, patient.etat_sante] for patient in patients]
+print(tabulate(patient_data, headers=["Nom", "Maladie", "Argent", "Poche", "État de Santé"], tablefmt="grid"))
+
+# Affichage des informations du docteur et des patients
 print("| Nom       | Argent | Cabinet | Diagnostique | Patient In | Patient Out |")
 print("|-----------|--------|---------|--------------|------------|-------------|")
-print(f"| {debugger.nom} | {debugger.argent} | {debugger.cabinet} | {debugger.diagnostique} | "
-      f"{debugger.patient_in} | {debugger.patient_out} |")
 
 #Simulation
-debugger.patient_in = marcus
-debugger.diagnostiquer(marcus)
-debugger.sortie_patient()
+for patient in patients:
+    debugger.patient_in = patient
+    debugger.diagnostiquer(patient)
+    debugger.sortie_patient()
+    print("| Nom       | Argent | Cabinet | Diagnostique | Patient In | Patient Out |")
+    print("|-----------|--------|---------|--------------|------------|-------------|")
+    doctor_data = [[debugger.nom, debugger.argent, debugger.cabinet, debugger.diagnostique, debugger.patient_in, debugger.patient_out]]
+    print(tabulate(doctor_data, headers=["Nom", "Argent", "Cabinet", "Diagnostique", "Patient In", "Patient Out"], tablefmt="grid"))
+
+for patient in patients:
+    patient.poche = traitements_patients[patients.index(patient)]
+    pharmacie.vendre_traitement(patient)
